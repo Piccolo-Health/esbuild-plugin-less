14a8fd62b1243209f8b70425abeb6361d0f9c333
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2020-11-30 06:38:50 +1000

    feat(*): Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# esbuild-plugin-less
+
+A Less loader for [esbuild](https://esbuild.github.io/). Compiles Less to CSS.
+
+## Install
+
+Install both this plugin and [esbuild](https://github.com/evanw/esbuild)
+
+An example build script looks like
+
+```javascript
+const esbuild = require('esbuild);
+const lessPlugin = require('esbuild-plugin-less').default;
+
+esbuild
+  .build({
+    entryPoints: ['app.js'],
+    bundle: true,
+    outfile: 'out.js',
+    plugins: [
+      lessPlugin({
+        javascriptEnabled: true,
+      }),
+    ],
+  })
+  .catch(() => process.exit(1));
+```
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import less from 'less';
+import { Plugin, PluginBuild, OnLoadOptions, OnLoadArgs } from 'esbuild';
+
+export interface LessPluginOptions {}
+
+export const defaultOptions: LessPluginOptions = {
+  sourceMap: true,
+  plugins: [],
+  relativeUrls: true,
+};
+
+const buildOptions: OnLoadOptions = {
+  filter: /\.less$/,
+};
+
+export default (options: LessPluginOptions): Plugin => ({
+  name: 'less',
+  setup(build: PluginBuild) {
+    build.onLoad(buildOptions, async (args: OnLoadArgs) => {
+      const filename = args.path;
+
+      const lessOptions = Object.assign(defaultOptions, options, {
+        filename: args.path,
+      });
+
+      try {
+        const data = await fs.promises.readFile(filename, 'utf8');
+        const result = await less.render(data, lessOptions);
+        return { contents: result.css, loader: 'css' };
+      } catch (error) {
+        return { errors: [{ text: error.message }] };
+      }
+    });
+  },
+});
